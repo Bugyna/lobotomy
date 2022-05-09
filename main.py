b59ad65090e33d3208a84d08834a878b6cdaebd5
Bugyna <matejbugy@gmail.com>
2022-05-09 22:23:21 +0200

    yatta

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ class TOKEN:
 		return self.c == cond
 
 	def __str__(self):
+		# return f"{self.c} : {self.type}"
+		# if (self.type == "IDENTIFIER"): return f"{self.c} : {self.type}"
+		if (self.c == "\n"): return f"{self.type}"
 		return f"{self.c} : {self.type}"
 
 	def __repr__(self):
@@ -89,6 +92,7 @@ def empty_expr():
 def parse(tokens):
 
 	expr = empty_expr()
+	expr_ret = []
 
 	p_count = 0
 	i = 0
@@ -96,34 +100,49 @@ def parse(tokens):
 	column = 0
 	args = False
 	arg_p_count = None
+	print("parsing: ", tokens, len(tokens))
 	while i < len(tokens):
 		t = tokens[i]
 	# for t in tokens:
 		if (t == "("):
 			if (args):
+				print("i before: ", i, tokens[i])
 				tmp_expr, tmp_i = parse(tokens[i:])
-				i += tmp_i
+				i += tmp_i - 1
+				print("i after: ", i, tokens[i])
+				print("tmp_expr: ", tmp_expr)
 				expr["args"].append(tmp_expr)
+				print("after tmp: ", expr, p_count, arg_p_count)
 				
 			elif (expr["keyword"]):
 				arg_p_count = p_count
 				args = True
 
-			else:
-				p_count += 1
+			p_count += 1
+
 
 		elif (t == ")"):
 			p_count -= 1
+			# print("aaa: ", expr, p_count)
 			if (arg_p_count == p_count):
+				print("aaaa", arg_p_count, p_count, line, column, i, expr)
 				arg_p_count = None
 				args = False
 				# print(expr)
 
-			if (p_count == 0):
-				# return expr, i
+			if (p_count == 1):
+				args = False
+				arg_p_count = None
+				expr_ret.append(expr)
+				print("happens", expr)
+				# print("expr_ret: ", expr_ret)
+				expr = empty_expr()
+				# break
+
+			elif (p_count == 0):
+				expr_ret.append(expr)
+				print("breaking: ", i)
 				break
-				# eval_expr(expr)
-				# expr = empty_expr()
 
 
 		elif (t == "\n"):
@@ -143,19 +162,23 @@ def parse(tokens):
 					expr["keyword"] = t.c
 
 		if (p_count < 0):
-			 raise Exception(f"too many right parenthesis {line}.{i}")
+			 raise Exception(f"too many right parenthesis {line}.{column} [{i}: {tokens[i].type}]")
 
 		i += 1
 		column += 1
 
-	if (p_count > 0):
-		 raise Exception(f"too many left parenthesis {line}.{i}")
+	# if (p_count > 0):
+		# print("pcount: ", p_count)
+		# raise Exception(f"too many left parenthesis {line}.{column} [{i}: {tokens[i].type}]")
 
-	return expr, i
+	print("end: ", expr_ret)
+	if (len(expr_ret) == 1): expr_ret = expr_ret[0]
+	return expr_ret, i
 
 
 
 def eval_expr(expr):
+	pass
 	# for 
 	# if (expr["keyword"] == "add"):
 		# print(inbuilt_add(expr["args"]))
@@ -166,13 +189,19 @@ def inbuilt_add(args):
 	return sum(args)
 
 
-t = """
-(add t (2 (add (4 4))))
-"""
-# t = """
-# (add t(2 2))
-# """
+t = """(
+	(minus (2 2))
+	(add t (2 (add (4 4)))
+	(minus (2 2))
+
+)"""
+
+
+# t = """(
+	# (add t (2 (add (4 4))))
+	# (add n (3 (add (5 5))))
+# )"""
+
+print(t)
 print("lex: ", tokenize(t))
 print("parse: ", parse(tokenize(t)))
-
-
