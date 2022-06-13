d80422ac1902fd9b995b4139b748dd7783f7c6bf
Bugyna <matejbugy@gmail.com>
2022-06-13 23:42:19 +0200

    b

--- src/main.c
+++ src/main.c
@@ -1,89 +1,5 @@
-#include "tokenize.c"
+#include "tokenize.h"
 
-enum {
-	T_INT=0,
-	T_BOOL,
-	T_CHAR,
-	T_STR,
-	T_DOUBLE,
-};
-
-
-int get_type_size(int type)
-{
-	switch (type) {
-		case T_INT:
-			return 4;
-		
-		case T_BOOL:
-			return 1;
-		
-		case T_CHAR:
-			return 1;
-
-		case T_STR:
-			return 8;
-
-		case T_DOUBLE:
-			return 8;
-	}
-
-	return 0;
-}
-
-typedef struct
-{
-	int type;
-	int size;
-
-	union {
-		int value;
-		char c;
-		char* str;
-		double d;
-	} val;
-} OBJ;
-
-
-OBJ create_obj(int value)
-{
-	OBJ obj;
-	obj.type = T_INT;
-	obj.size = value;
-	obj.val.value = value;
-	return obj;
-}
-
-
-typedef struct
-{
-	int len;
-	int8_t data[2];
-} BIG_NUM;
-
-void bignum_from_int(BIG_NUM* bn, int n)
-{
-	// 0000 0000 0000 0000 0000 0000 0000 1000
-	// 0x0 0x0 0x0 0x8
-	// 8
-
-	// 0000 0000 0000 0000
-	printf("c: %d, %d\n", n, n >> 8);
-	bn->data[1] = n;
-	bn->data[0] = n >> 8;
-}
-
-
-char* bignum_to_str(BIG_NUM bn)
-{
-	char* s = malloc(16);
-	// for (int i = 15; i > 0; i--) {
-	// snprintf(s, 16, "%d", bn.data[1]);
-	snprintf(s, 16, "%d", bn.data[1] & (bn.data[0] << 8));
-	// }
-
-	return s;
-}
 
 int main (int argc, char* argv[]) {
 
@@ -91,26 +7,8 @@ int main (int argc, char* argv[]) {
 		printf("no: %s\n", *argv);
 	}
 
-	BIG_NUM bn;
-	bignum_from_int(&bn, 128);
-	printf("%d, %d, %s", bn.data[0], bn.data[1], bignum_to_str(bn));
+	tokenize("(+ 2 2)");
 
-	// char t[] = "(+ 2 2)";
-	// printf("t: %s, %ld\n", t, sizeof(long double));
-	// for (int i = 0; i < sizeof t; i++) {
-		// printf("c: %c, %d\n", t[i], i);
-
-	// }
-
-	// printf("\n");
-	// long long double* test = calloc(1, 64);
-	// intmax_t test = pow(2, 1023);
-	// *test = pow(2, 1024);
-	// printf("test: %Le\n", test);
-	// printf("test: %Le\n", LDBL_MAX);	
-	// printf("test: %Le\n", (long double)pow(2, 1024));
-	// printf("test: %f\n", pow(2, 1023));
-	// printf("test: %f\n", pow(2, 63));
 	
 
 	return 0;
--- src/tokenize.h
+++ src/tokenize.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <assert.h>
 #include <string.h>
-#include <malloc.h>
 #include <math.h>
 #include <float.h>
 #include <stdint.h>
@@ -26,11 +25,36 @@ enum
 	TT_BIN_LESS,
 	TT_BIN_GREATER,
 	TT_BIN_EQUAL,
-}
+};
 
+
+typedef struct
+{
+	int start, stop;
+	int type, len;
+	char* text;
+} TOKEN;
 
 
 void tokenize(const char text[])
 {
-	
+	TOKEN* tokens;
+	TOKEN token;
+	tokens = calloc(20, sizeof(TOKEN));
+	size_t len = strlen(text);
+	for (size_t i = 0; i < len; i++) {
+		if (text[i] == '(') {
+			token.start = i;
+			token.stop = i;
+			char t[2] = {text[i], '\0'};
+			token.text = t;
+			token.len = 2;
+			tokens[0] = token;
+		}
+	}
+
+	printf("t: %s\n", tokens[0].text);
 }
+
+
+
